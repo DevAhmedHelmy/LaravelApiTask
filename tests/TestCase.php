4b3ba44cb9544b3c39cac3ab0142161bc28fc135
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-12-25 23:12:23 +0200

    finish actions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    protected function siginIn($user = null)
+    {
+        $user = $user ?: factory('App\User')->create();
+        $this->actingAs($user);
+        return $user;
+    }
 }
