fa4632b928f3c2d2bdb1ee135c89acfbe8049bdd
DevAhmedHelmy <helmyahmed248@gmail.com>
2019-12-25 23:25:40 +0200

    --- Auto-Git Commit ---

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -8,5 +8,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CustomerTest extends TestCase
 {
-     
+     /** @test */
+    public function only_admin_can_create_customer()
+    {
+        // siginIN
+        $this->siginIn();
+        $this->post('/customers', $attributes = factory(\App\Customer::class)->raw(['user' => auth()->id()]));
+    }
 }
